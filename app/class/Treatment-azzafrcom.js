6f7cfa9cf0ad6f917af13e2441b02ee45e8cdd04
Fabrice <info@proglab.com>
2019-01-16 01:48:05 +0100

    fix com

--- app/class/Treatment-azzafrcom.js
+++ app/class/Treatment-azzafrcom.js
@@ -37,8 +37,30 @@ class Treatment {
         var oldNumPiece = null;
         var num = '';
 
+        var name = '';
+
+        var lines = '';
+
 
         $.each(text, function(index, value ) {
+
+            if (value.Libelle.substr(0, 17) == 'Urssaf Entreprise')
+            {
+                return true;
+            }
+
+            if (name == '')
+            {
+                name = value.Libelle.substr(11, value.Libelle.length);
+                console.log('init ' + name);
+
+            }
+
+            const dateStr = value.Date;
+            const date = dateStr.split('/');
+
+
+
             if (i < 9)
             {
                 num = '000' + i;
@@ -56,113 +78,60 @@ class Treatment {
                 num = i;
             }
 
-            const dateStr = value.Date;
-            const date = dateStr.split('/');
-
-
-            if (value.CompteGeneral == '622200')
+            if (value.CompteGeneral != '')
             {
-                if (i > 0)
-                {
-                    body += "\r\n}\r\n";
-                }
-                console.log(headTemplate);
-                body += "Purchases:\r\n{\r\n";
+                //traitement
+                let FlagDC = value.Debit == 0 ? 'C' : 'D';
+                let VATCode = '0';
+                let GnrlID = value.Compte;
+
+                sum = FlagDC == 'C' ? parseFloat(sum) - parseFloat(parseFloat(value.Credit.replace(',', '.')).toFixed(2)) : parseFloat(sum) + parseFloat(parseFloat(value.Debit.replace(',', '.')).toFixed(2));
+
+                let line =  lineTemplate.replace('[[GnrlID]]', GnrlID);
+                line =  line.replace('[[AnalID]]', '');
+                line =  line.replace('[[VATCode]]', VATCode);
+                line =  line.replace('[[Comment]]', 'NCOM' + ' ' + parseInt(date[1]) + '/' + parseInt(date[2]));
+                line =  line.replace('[[FlagDC]]', FlagDC);
+                line =  line.replace('[[CrcyID]]', 'EUR');
+                line =  line.replace('[[AmountCrcy]]', FlagDC == 'C' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
+                line =  line.replace('[[AmountCrcyDoc]]', FlagDC == 'C' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
+                line =  line.replace('[[AmountCrcyBase]]', FlagDC == 'C' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
+                line =  line.replace('[[AmountVATCrcyDoc]]', FlagDC == 'C' ? (Math.round(parseFloat(value.Credit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2) :  (Math.round(parseFloat(value.Debit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2));
+                line =  line.replace('[[AnalRecordTag]]', '');
+                line =  line.replace('[[AnalQuantity]]', '0');
+                line =  line.replace('[[AnalPercent]]', '0.00');
+                lines += line;
+
+            }
+            else
+            {
+
 
                 let head =  headTemplate.replace('[[JrnlID]]', 'PRE');
                 head =  head.replace('[[DocType]]', 1);
+                head =  head.replace('[[DocNumber]]', date[1] + num);
+                head =  head.replace('[[SuppID]]', value.Compte.replace('AZ_', ''));
                 head =  head.replace('[[Comment]]', 'NCOM' + ' ' + parseInt(date[1]) + '/' + parseInt(date[2]));
                 head =  head.replace('[[PeriodID]]', parseInt(date[1]));
                 head =  head.replace('[[DateDoc]]',dateStr+' 00:00:00');
                 head =  head.replace('[[DateDue]]', dateStr+' 00:00:00');
                 head =  head.replace('[[Piece]]', '');
                 head =  head.replace('[[CrcyDoc]]', "EUR");
-                head =  head.replace('[[AmountCrcyDoc]]', parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                body += head;
-                oldNumPiece = value.Compte;
+                head =  head.replace('[[AmountCrcyDoc]]', parseFloat(sum.toFixed(2)));
+                body += "Purchases:\r\n{\r\n" + head + lines + "\r\n}\r\n";
+                lines = '';
+
+                console.log('TOTAL' + sum);
+                //total
+                name = '';
+
+                sum = 0;
 
             }
-            else if (value.CompteGeneral != '')
-            {
-                if (value.Libelle.substr(0, 17) == 'Urssaf Entreprise')
-                {
-                    return true;
-                }
 
-                let FlagDC = value.Debit == 0 ? 'D' : 'C';
-
-                console.log(value.Compte);
-
-                let VATCode = '0';
-
-
-                let GnrlID = value.Compte;
-
-                sum = FlagDC == 'C' ? parseFloat(sum) + parseFloat(parseFloat(value.Credit.replace(',', '.')).toFixed(2)) : parseFloat(sum) + parseFloat(parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                sum = FlagDC == 'C' ? parseFloat(sum) + parseFloat(Math.round(parseFloat(value.Credit.replace(',', '.')*VATCode/100)*100)/100) : parseFloat(sum) + parseFloat(Math.round(parseFloat(value.Debit.replace(',', '.')*VATCode/100)*100)/100);
-
-                let line =  lineTemplate.replace('[[GnrlID]]', oldNumPiece);
-                line =  line.replace('[[AnalID]]', '');
-                line =  line.replace('[[VATCode]]', VATCode);
-                line =  line.replace('[[Comment]]', 'NCOM' + ' ' + parseInt(date[1]) + '/' + parseInt(date[2]));
-                line =  line.replace('[[FlagDC]]', FlagDC);
-                line =  line.replace('[[CrcyID]]', 'EUR');
-                line =  line.replace('[[AmountCrcy]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountCrcyDoc]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountCrcyBase]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountVATCrcyDoc]]', FlagDC == 'D' ? (Math.round(parseFloat(value.Credit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2) :  (Math.round(parseFloat(value.Debit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2));
-                line =  line.replace('[[AnalRecordTag]]', '');
-                line =  line.replace('[[AnalQuantity]]', '0');
-                line =  line.replace('[[AnalPercent]]', '0.00');
-                body += line;
-
-                oldNumPiece = value.Compte;
-            }
-            else
-            {
-                const dateStr = value.Date.replace('/', '-').replace('/', '-').replace('2017', '17');
-                const date = dateStr.split('-');
-                body =  body.replace('[[DocNumber]]', date[1] + num);
-
-
-                if (value.CompteTiers == '')
-                {
-                    return true;
-                }
-
-                body = body.replace('[[SuppID]]', value.Compte.replace('AZ_', ''));
-
-
-                let FlagDC = value.Debit == 0 ? 'D' : 'C';
-
-                console.log(value.Compte);
-
-                let VATCode = '0';
-                let GnrlID = value.Compte;
-
-                sum = FlagDC == 'C' ? parseFloat(sum) + parseFloat(parseFloat(value.Credit.replace(',', '.')).toFixed(2)) : parseFloat(sum) + parseFloat(parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                sum = FlagDC == 'C' ? parseFloat(sum) + parseFloat(Math.round(parseFloat(value.Credit.replace(',', '.')*VATCode/100)*100)/100) : parseFloat(sum) + parseFloat(Math.round(parseFloat(value.Debit.replace(',', '.')*VATCode/100)*100)/100);
-
-                let line =  lineTemplate.replace('[[GnrlID]]', oldNumPiece);
-                line =  line.replace('[[AnalID]]', '');
-                line =  line.replace('[[VATCode]]', VATCode);
-                line =  line.replace('[[Comment]]', 'NCOM' + ' ' + parseInt(date[1]) + '/' + parseInt(date[2]));
-                line =  line.replace('[[FlagDC]]', FlagDC);
-                line =  line.replace('[[CrcyID]]', 'EUR');
-                line =  line.replace('[[AmountCrcy]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountCrcyDoc]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountCrcyBase]]', FlagDC == 'D' ? parseFloat(value.Credit.replace(',', '.')).toFixed(2) : parseFloat(value.Debit.replace(',', '.')).toFixed(2));
-                line =  line.replace('[[AmountVATCrcyDoc]]', FlagDC == 'D' ? (Math.round(parseFloat(value.Credit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2) :  (Math.round(parseFloat(value.Debit.replace(',', '.')*VATCode/100)*100)/100).toFixed(2));
-                line =  line.replace('[[AnalRecordTag]]', '');
-                line =  line.replace('[[AnalQuantity]]', '0');
-                line =  line.replace('[[AnalPercent]]', '0.00');
-                body += line;
-
-            }
             i++;
         });
         body = body.replace('[[AmountCrcyDoc]]', parseFloat(sum).toFixed(2));
-        body += '}';
         console.log('Transformation ending...');
         return this.template+body;
     }
