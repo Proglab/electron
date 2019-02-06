58ee9193414ed3fec5505330ee424f16bb98c123
Fabrice <info@proglab.com>
2019-02-06 19:17:01 +0100

    fix grunt

--- GruntFile.js
+++ GruntFile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         rename: {
             main: {
                 files: [
-                    {src: ['dist/moka2popsy Setup *.*'], dest: 'dist/moka2popsy-Setup-<%= pkg.version %>.exe'},
+                    {src: ['dist/moka2popsy Setup <%= pkg.version %>.exe'], dest: 'dist/moka2popsy-Setup-<%= pkg.version %>.exe'},
                 ]
             }
         },
